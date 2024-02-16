c4ec2276fea273fac80c5f13dec4835b7b62781c
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-02-16 08:17:47 +0100

    add fonts, init styling + init header.

---
+++ src/components/header.tsx
@@ -0,0 +1,21 @@
+const Header = () => {
+    return(
+        <header>
+            <h1>Design Fest Gent</h1>
+            <div>
+                <h1 className={"main-title"}>All The Small Things</h1>
+                <p className={"main-title_sub"}>March 20-30, 2025</p>
+            </div>
+            <nav>
+                <h1>about</h1>
+                <div>
+                    <h1>NL</h1>
+                    <h1>EN</h1>
+                    <h1>FR</h1>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
