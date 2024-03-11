70360ede1e44bc1cfebb4142a2748219a2256050
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-03-11 15:30:11 +0100

    add link to open-call

---
+++ src/pages/open-call.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/header";
+import Footer from "../components/footer";
+
+const openCall = () => {
+    return (
+        <div>
+            <Header/>
+            <section className={"container"}>
+                <h1>open call</h1>
+            </section>
+            <Footer/>
+        </div>
+    )
+}
+
+export default openCall;
